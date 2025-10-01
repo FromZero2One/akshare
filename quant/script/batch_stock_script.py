f52dba46d36eaf45cecb2ca3e0e9bb3919fbf877
Administrator <hk269733118@qq.com>
2025-10-01 12:26:56 +0800

    字段调整

--- quant/script/batch_stock_script.py
+++ quant/script/batch_stock_script.py
@@ -5,14 +5,14 @@ import quant.utils.db_orm as db_orm
 
 def main():
     # 千股千评
-    ads.stock_comment_em_orm(reBuild=True)
+    # ads.stock_comment_em_orm(reBuild=True)
     symbol = "600000"
     # 获取所有股票名称
     # all_stock_name_df = db_orm.get_mysql_data_to_df(orm_class=StockNameEntity)
     # 获取指定股票历史行情数据
     # ads.stock_zh_a_hist_orm(reBuild=True, symbol=symbol, start_date="20220101", end_date="20221231")
     # 估值分析
-    # ads.stock_value_em_orm(symbol='', TRADE_DATE="2025-09-25", reBuild=True)
+    ads.stock_value_em_orm(symbol='', TRADE_DATE="2025-09-26", reBuild=False)
 
 
 if __name__ == '__main__':
