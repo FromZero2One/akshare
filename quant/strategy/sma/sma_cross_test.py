30d9af4bb845d3ea8c21a82ae23386a7959a0922
From Zero 2 One <hk269733118@qq.com>
2025-10-24 12:17:49 +0800

    清理数据&& 调整结构

--- quant/strategy/sma/sma_cross_test.py
+++ quant/strategy/sma/sma_cross_test.py
@@ -3,7 +3,7 @@ import time
 import quant.utils.db_orm as db_orm
 from quant.entity.StockHistoryDailyInfoEntity import StockHistoryDailyInfoEntity
 from quant.entity.StockNameEntity import StockNameEntity
-from quant.entity.stock_data_save_script import stoch_zh_a_hist_orm_incremental
+from quant.entity.script.stock_data_save_script import stoch_zh_a_hist_orm_incremental
 from quant.strategy.sma.SmaCross import SmaCross
 from quant.strategy.sma.SmaStrategyScript import strategy_back_trader
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         print("没有获取到股票列表")
 
     # 只拉取股票历史数据 不进行回测
-    only_pull = False
+    only_pull = True
     # 是否重新构建回测结果 新添加字段时 只执行一次即可
     reBuildResult = False
     for index, row in df_name_list.iterrows():
@@ -24,16 +24,16 @@ if __name__ == '__main__':
         history_df = db_orm.get_mysql_data_to_df(orm_class=StockHistoryDailyInfoEntity, symbol=symbol, adjust=adjust)
         # 如果没有历史数据 先拉取数据
         if history_df.empty:
-            print(f"股票 {symbol} 没有历史数据,先拉取数据")
+            print(f"股票 {symbol}[{stock_name}] 没有历史数据,先拉取数据")
             # 最终暂停时间为2秒加上0-2秒的随机值，即2-4秒之间的随机延迟
-            time.sleep(2 + int(3 * time.time()) % 3)
+            # time.sleep(2 + int(3 * time.time()) % 3)
             stoch_zh_a_hist_orm_incremental(symbol=symbol, adjust=adjust, isDel=False)
         else:
             if only_pull:
-                print(f"股票 {symbol} 已有历史数据,跳过拉取")
+                print(f"股票 {symbol}[{stock_name}] 已有历史数据,跳过拉取")
                 continue
             else:
-                print(f"开始回测股票: {symbol}")
+                print(f"开始回测股票: {symbol}[{stock_name}]")
                 strategy_back_trader(tb_df=history_df, strategy=SmaCross, symbol=symbol, stock_name=stock_name,
                                      adjust=adjust, reBuildResult=reBuildResult)
     print(f"完成回测所有股票")
